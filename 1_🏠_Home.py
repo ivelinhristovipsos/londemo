346135cf859ca09ddaaed3af814ba16399f0ddaa
Ivelin Hristov <ivelin.hristov@ipsos.com>
2025-03-19 17:33:53 +0200

    Refactor Home page layout and add style helpers for improved UI consistency

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -1,25 +1,16 @@
+import pandas as pd
 import streamlit as st
 
 from data.dummy_data import dummy_df, raw_dummy_data
-import pandas as pd
-import numpy as np
-
-from modules.helper import add_header, global_page_style
-
-raw_df = raw_dummy_data()
+from modules.style_helpers import add_header, global_page_style
 
 st.set_page_config(page_title="Home", layout="wide", page_icon="🏠")
 
-
+raw_df = raw_dummy_data()
 
 add_header("Demo Dashboard", 2)
 
-
-# Set page configuration
-
-# Sidebar navigation
 with st.sidebar:
-
     st.sidebar.title("Filters")
 
     country_options = sorted(raw_df["Country"].unique())
@@ -42,7 +33,6 @@ with st.sidebar:
 # Top-level metrics container
 
 with st.container(border=True) as cont:
-
     options = ["1d", "7d", "14d", "max"]
     selection = st.segmented_control(
         label="date_filter",
@@ -53,12 +43,13 @@ with st.container(border=True) as cont:
         label_visibility="collapsed",
     )
 
-    # LOGIC: 
+    # LOGIC:
     back_days = None
     if selection in ["1d", "7d", "14d"]:
         back_days = int(selection[:-1])
-        period_selection_mask = raw_df["Date"] >= raw_df["Date"].max() - pd.Timedelta(days=back_days)
-    
+        period_selection_mask = raw_df["Date"] >= raw_df["Date"].max() - pd.Timedelta(
+            days=back_days
+        )
 
     # Apply filters
     if selected_group:
@@ -71,6 +62,8 @@ with st.container(border=True) as cont:
     df = dummy_df(raw_df)
 
     col1, col2, col3, col4 = st.columns(4)
+    
+    
     with col1:
         with st.container():
             valid_completes = raw_df["Valid Completes"].sum()
@@ -78,19 +71,17 @@ with st.container(border=True) as cont:
             st.metric("Valid Completes", valid_completes, "+275 (mocked)", border=True)
     with col2:
         with st.container():
-            rr_mean = round(raw_df['Response Rate'].mean(), 1)
-            st.metric(
-                "Response Rate", f"{rr_mean} %", "-24%", border=True
-            )
+            rr_mean = round(raw_df["Response Rate"].mean(), 1)
+            st.metric("Response Rate", f"{rr_mean} %", "-24%", border=True)
     with col3:
         with st.container():
-            se_mean = raw_df['Sample Exhaustion'].mean()
+            se_mean = raw_df["Sample Exhaustion"].mean()
             st.metric(
                 "Sample Exhaustion",
                 f"{round(se_mean, 1)} %",
                 "(10.75%)",
                 delta_color="inverse",
-                border=True
+                border=True,
             )
     with col4:
         with st.container():
@@ -103,24 +94,25 @@ with st.container(border=True) as cont:
                 border=True,
             )
 
-add_header("Country Level Breakdown", 4)
-edited_df = st.dataframe(
-    df,
-    use_container_width=True,
-    column_config={
-        "% Completion": st.column_config.ProgressColumn(
-            min_value=0, max_value=100, format="%d%%"
-        ),
-        "Response Rate": st.column_config.NumberColumn(format="%d%%"),
-        "Refusal Rate": st.column_config.NumberColumn(format="%d%%"),
-        "Sample Exhaustion": st.column_config.NumberColumn(format="%d%%"),
-        "Data Quality Issues": st.column_config.NumberColumn(format="plain"),
-    },
-    hide_index=True,
-    # selection_mode="single-row",
-    # key="data",
-    # on_select="rerun",
-)
+with st.container():
+    add_header("Country Level Breakdown", 4)
+    edited_df = st.dataframe(
+        df,
+        use_container_width=True,
+        column_config={
+            "% Completion": st.column_config.ProgressColumn(
+                min_value=0, max_value=100, format="%d%%"
+            ),
+            "Response Rate": st.column_config.NumberColumn(format="%d%%"),
+            "Refusal Rate": st.column_config.NumberColumn(format="%d%%"),
+            "Sample Exhaustion": st.column_config.NumberColumn(format="%d%%"),
+            "Data Quality Issues": st.column_config.NumberColumn(format="plain"),
+        },
+        hide_index=True,
+        # selection_mode="single-row",
+        # key="data",
+        # on_select="rerun",
+    )
 
 if __name__ == "__main__":
     global_page_style()
