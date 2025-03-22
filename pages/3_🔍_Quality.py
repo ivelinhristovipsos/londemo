230a420eca14e44324d9e434e5b632f94c2f8207
Daniel Dimitrov <Daniel.Dimitrov@ipsos.com>
2025-03-22 08:50:36 +0200

    added couple of pages, defining the full scope

--- pages/3_🔍_Quality.py
+++ pages/3_🔍_Quality.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
 # Set page configuration
-st.set_page_config(page_title='Demographics', layout='wide', page_icon="📊")
+st.set_page_config(page_title='Quality', layout='wide', page_icon="🔍")
 from modules.style_helpers import add_header, global_page_style
 
-add_header("<img src='https://images1.ipsosinteractive.com/GOHBG/ISR/Admin/Reporting_Demo/images/client_logo.png' width='200px'/> <br/> Fieldwork Progress Dashboard - Demographics", 2)
+add_header("<img src='https://images1.ipsosinteractive.com/GOHBG/ISR/Admin/Reporting_Demo/images/client_logo.png' width='200px'/> <br/> Fieldwork Progress Dashboard - Quality Metrics", 2)
 
 st.logo('https://upload.wikimedia.org/wikipedia/en/a/a6/Ipsos_logo.svg', icon_image='https://upload.wikimedia.org/wikipedia/en/a/a6/Ipsos_logo.svg')
 
